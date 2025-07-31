8112df4b8746f7fcf43a7f7bc405236ec6b1179d
Raanan Peretz <47702617+raananp@users.noreply.github.com>
2025-07-31 19:51:52 +0300

    Update index.js

--- bike-rental-dockerized-with-nginx/frontend/src/index.js
+++ bike-rental-dockerized-with-nginx/frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './Home';  // assuming your Home component is in Home.js
-import './index.css';        // optional if you have styling
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
