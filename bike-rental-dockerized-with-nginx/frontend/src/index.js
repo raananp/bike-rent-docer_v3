7ceb0c5b90ca6b59662c16ebaacd315929046d5c
Raanan Peretz <47702617+raananp@users.noreply.github.com>
2025-08-01 15:38:08 +0300

    Update index.js

--- bike-rental-dockerized-with-nginx/frontend/src/index.js
+++ bike-rental-dockerized-with-nginx/frontend/src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline, GlobalStyles } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <CssBaseline />
+    <GlobalStyles
+      styles={{
+        'html, body, #root': {
+          height: '100%',
+          width: '100%',
+          margin: 0,
+          padding: 0,
+          backgroundColor: '#121212', // Dark background
+          overflowX: 'hidden',
+          fontFamily: 'Roboto, sans-serif',
+        },
+        '*': {
+          boxSizing: 'border-box',
+        },
+        a: {
+          textDecoration: 'none',
+          color: 'inherit',
+        },
+      }}
+    />
     <App />
   </BrowserRouter>
 );
