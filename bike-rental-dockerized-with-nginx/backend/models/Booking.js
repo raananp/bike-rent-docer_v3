ced76ac81b0e652306113ba688925d4656df15d2
Raanan Peretz <47702617+raananp@users.noreply.github.com>
2025-08-01 18:23:14 +0300

    Update Booking.js

--- bike-rental-dockerized-with-nginx/backend/models/Booking.js
+++ bike-rental-dockerized-with-nginx/backend/models/Booking.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  date: { type: String, required: true },
-  duration: { type: Number, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  startDateTime: { type: String, required: true },
+  endDateTime: { type: String, required: true },
+  numberOfDays: { type: Number, required: true },
   bike: { type: String, required: true },
   insurance: { type: Boolean, default: false },
   licenseUploaded: { type: Boolean, default: false },
